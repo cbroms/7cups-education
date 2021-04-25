a2c9ad6068013ba12c51ef20ca421404a9ac04fd
Christian Broms <cb@christianbroms.com>
2021-04-25 17:41:59 -0400

    fix complete to fetch user type on mount

--- pages/scenario/[scenarioId]/complete.js
+++ pages/scenario/[scenarioId]/complete.js
@@ -6,20 +6,21 @@ import Chat from "../../../components/Chat";
 
 const Complete = () => {
   const [messages, setMessages] = useState([]);
+  const [user, setUser] = useState("listener");
 
   useEffect(() => {
     setMessages(getMessages());
+    setUser(getScenario() === "member" ? "listener" : "member");
   }, []);
   return (
     <React.Fragment>
       <h1>Scenario Complete</h1>
       <p>
         By staying respectful, referencing your community guidelines, and
-        letting a {getScenario() === "member" ? "listener" : "member"} know when
-        their behavior is inappropriate, you can stay in control when difficult
-        situations arise. If it comes to it, submitting a detailed report can
-        help ensure that those who routinely behave badly are removed from the
-        community.{" "}
+        letting a {user} know when their behavior is inappropriate, you can stay
+        in control when difficult situations arise. If it comes to it,
+        submitting a detailed report can help ensure that those who routinely
+        behave badly are removed from the community.{" "}
       </p>
       <h3>Here's how your chat went</h3>
       <Chat show7Cups chat={messages}></Chat>
