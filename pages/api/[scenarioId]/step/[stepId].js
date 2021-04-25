0ad8f142f32265f19d9e1c889e13c09caaa537fc
Christian Broms <cb@christianbroms.com>
2021-04-25 16:06:38 -0400

    fix duplicate res issue

--- pages/api/[scenarioId]/step/[stepId].js
+++ pages/api/[scenarioId]/step/[stepId].js
@@ -2,18 +2,18 @@ import listenerDb from "../../../../db/listenerDb";
 import memberDb from "../../../../db/memberDb";
 
 export default (req, res) => {
-  let res = {};
+  let data = {};
 
   try {
     if (req.params.scenarioId === "listener") {
-      res = listenerDb.steps[req.params.stepId];
+      data = listenerDb.steps[req.params.stepId];
     } else if (req.params.scenarioId === "member") {
-      res = memberDb.steps[req.params.stepId];
+      data = memberDb.steps[req.params.stepId];
     } else {
       res.status(404).json({ error: "Not found" });
     }
 
-    res.status(200).json(res);
+    res.status(200).json(data);
   } catch (e) {
     res.status(500).json({ error: "Server error" });
   }
